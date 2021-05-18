c2f1c1e9618eb4cdbabcbdf47675510b4acc4d18
mbs0221 <mbs2021@163.com>
2021-05-18 11:05:13 +0800

    add chosen node in dts

--- src/main/scala/diplomacy/Resources.scala
+++ src/main/scala/diplomacy/Resources.scala
@@ -437,8 +437,10 @@ object ResourceAnchors
     }
   }
 
-  val chosen = new DeviceSnippet {
-    def describe() = Description("chosen", Map())
+  val chosen = new Device {
+    def describe(resources: ResourceBindings): Description = {
+      Description("chosen", Map())
+    }
   }
 
   val cpus = new Device {
